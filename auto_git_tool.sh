8c9d24963a4925d1f8436dd09c56d3dc1d18e974
kanikama0601 <kanikama0601school@gmail.com>
2023-04-30 00:47:40 +0900

    M auto_git_tool.sh

--- auto_git_tool.sh
+++ auto_git_tool.sh
@@ -3,6 +3,6 @@ while :; do
 	git -C /home/kanikama0601/for_test pull
 	git -C /home/kanikama0601/for_test add .
 	git -C /home/kanikama0601/for_test commit -m "auto commit"
-	git -C /home/kanikama0601/for_test push
+	git -C /home/kanikama0601/for_test push --force
 	sleep 120
 done
